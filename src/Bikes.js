a357c1651c2d8b5f73f6e9895ba5f5608feac65d
Eric Butera <eric@ericbutera.com>
2022-06-29 21:09:44 -0400

    docs: add some fine literature

--- src/Bikes.js
+++ src/Bikes.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './Bikes.css';
+import React, { useState, useEffect } from "react";
+import "./Bikes.css";
 
 function Bikes() {
   const [bikes, setBikes] = useState([]);
 
   useEffect(() => {
+    // https://reactjs.org/docs/faq-ajax.html
     const fetchData = async () => {
-      fetch(
-        '/bikes.json',
-      ).then(response => response.json())
-      .then(data => {
-        setBikes(data.bikes)
-      });
+      fetch("/bikes.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setBikes(data.bikes);
+        });
     };
 
     fetchData();
   }, []);
 
   return (
-    <ul id='bikes'>
-      {bikes.map(item => (
+    <ul id="bikes">
+      {bikes.map((item) => (
         <li key={item.id}>
           <span>{item.brand}</span>
           <span>{item.model}</span>
-          <img src={"/i/" + item.image} alt="{item.model}"/>
+          <img src={"/i/" + item.image} alt="{item.model}" />
         </li>
       ))}
     </ul>
